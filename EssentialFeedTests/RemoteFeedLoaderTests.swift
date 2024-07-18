e5510fc502d5eaa21f5f4d3fedfb605660bade33
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2024-07-19 09:06:12 +1200

    Update explicit JSON data for non 200 HTTP response tests

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -65,7 +65,8 @@ class RemoteFeedLoaderTests: XCTestCase {
         // Act
         samples.enumerated().forEach { index, code in
             expect(sut, toCompleteWith: .failure(.invalidData), when: {
-                client.complete(withStatusCode: code, at: index)
+                let json = makeItemsJSON([])
+                client.complete(withStatusCode: code, data: json, at: index)
             })
         }
         
@@ -180,7 +181,7 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index], 
                 statusCode:code,
